b72dd524447d0221623444637c37d2bb599b9bc8
909009-yqx <y2558924262@163.com>
2021-03-27 16:25:13 +0800

    简单计算器

---
+++ 20210322计算器/20210322计算器/Count.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20210322计算器
+{
+    class Count
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine("相加为：{0}", x + y);
+        }
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine("相减为：{0}", x - y);
+        }
+        public void Mul(int x, int y)
+        {
+            Console.WriteLine("相乘为：{0}", x * y);
+        }
+        public void Div(int x, int y)
+        {
+            Console.WriteLine("相除为：{0}", x / y);
+        }
+        public bool Equals(int x, int y)
+        {
+            if (x == y)
+            {
+                Console.WriteLine("两整数相等");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("两整数不等");
+                return false;
+            }
+        }
+    }
+}
---
+++ 20210322计算器/20210322计算器/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20210322计算器
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Count c = new Count();
+            
+            while (true)
+            {
+                int x, y;
+                char t;
+                Console.WriteLine("==================================================");
+                Console.WriteLine("输入两个整数：");
+                x = Convert.ToInt32(Console.ReadLine());
+                y = Convert.ToInt32(Console.ReadLine());
+                c.Equals(x, y);
+                Console.WriteLine("输入将要进行的操作：+,-,*,/,0");
+                t = Convert.ToChar(Console.ReadLine());
+                switch (t)
+                {
+                    case '0':
+                        return;
+                    case '+':
+                        c.Add(x, y);
+                        break;
+                    case '-':
+                        c.Sub(x, y);
+                        break;
+                    case '*':
+                        c.Mul(x, y);
+                        break;
+                    case '/':
+                        try
+                        {
+                            c.Div(x, y);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("分母不能为零");
+                        }
+                        finally
+                        {
+                            Console.WriteLine("结束");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("无此项操作！");
+                        break;
+
+                }
+            }
+            
+        }
+    }
+}
